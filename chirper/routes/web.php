43cf8d76198cab4ab0e847717e98b6dcd81cb58f
NetworkerLurker <78961523+justice-bole@users.noreply.github.com>
2023-02-16 14:08:43 -0500

    Add ability to create chirps

--- chirper/routes/web.php
+++ chirper/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('chirps', ChirpController::class)
+    ->only(['index', 'store'])
+    ->middleware(['auth', 'verified']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
