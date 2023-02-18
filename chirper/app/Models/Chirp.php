d08b16deb7f9fdce433621b34ddaad72a86edf99
NetworkerLurker <78961523+justice-bole@users.noreply.github.com>
2023-02-17 22:40:11 -0500

    Add chirp notifications

--- chirper/app/Models/Chirp.php
+++ chirper/app/Models/Chirp.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Events\ChirpCreated;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
@@ -10,12 +11,17 @@ class Chirp extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'message',
+    ];
+
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
+    ];
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
     
-    protected $fillable = [
-        'message',
-    ];
 }
